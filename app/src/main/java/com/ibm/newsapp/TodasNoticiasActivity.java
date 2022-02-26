1a7697dfc3c7414fb66b6ea66c8f0547f271041b
nathborges <nathi-borges@hotmail.com>
2022-02-26 18:56:26 -0300

    font: Add and setting Visby font. top menu: Add items, and line.

---
+++ app/src/main/java/com/ibm/newsapp/TodasNoticiasActivity.java
@@ -0,0 +1,14 @@
+package com.ibm.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodasNoticiasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todas_noticias);
+    }
+}
