c55e130eb503392088387f0dea4966bb6302ec84
nathalia pavani <nathi.pavani@live.com>
2022-03-03 15:58:13 -0300

    tela cadastro

--- app/src/main/java/com/ibm/newsapp/CadastroActivity.java
+++ app/src/main/java/com/ibm/newsapp/CadastroActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class CadastroActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
 }
