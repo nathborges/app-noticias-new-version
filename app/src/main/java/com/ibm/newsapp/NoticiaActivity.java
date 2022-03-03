8780f83cab38cab5ac5d008d4f8c39955685e3f4
Lívia Oliveira <livia.oliveira.almeida@hotmail.com>
2022-03-03 16:27:03 -0300

    tela de notícia selecionada.

--- app/src/main/java/com/ibm/newsapp/NoticiaActivity.java
+++ app/src/main/java/com/ibm/newsapp/NoticiaActivity.java
@@ -15,27 +15,26 @@ public class NoticiaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticia);
-        botaoSaibaMais = findViewById(R.id.bt_saiba);
-        botaoSupreenda = findViewById(R.id.bt_surpreenda);
+        //botaoSaibaMais = findViewById(R.id.bt_saiba);
+        //botaoSupreenda = findViewById(R.id.bt_surpreenda);
 
-        botaoSaibaMais.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent saibaMais = new Intent(getApplicationContext(), SaibaMaisActivity.class);
-                startActivity(saibaMais);
+        //botaoSaibaMais.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //   public void onClick(View view) {
+        //     Intent saibaMais = new Intent(getApplicationContext(), SaibaMaisActivity.class);
+        //     startActivity(saibaMais);
 
-            }
-        });
+        //   }
+        //});
 
-        botaoSupreenda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent it = new Intent(NoticiaActivity.this, SurpreendaActivity.class);
-                startActivity(it);
+        // botaoSupreenda.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        // public void onClick(View view) {
+        //    Intent it = new Intent(NoticiaActivity.this, SurpreendaActivity.class);
+        //   startActivity(it);
 
-            }
-        });
-
-    }
+        // }
+    };
 
 }
+
