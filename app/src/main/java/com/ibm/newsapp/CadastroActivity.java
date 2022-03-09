ed6b539ac3ef6b7bc898f0f4ecd947ebc72bc776
˜Carol˜ <eriushirobira@hotmail.com>
2022-03-09 13:02:21 -0300

    Connecting LoginScreen authentication to FirebaseAuth

--- app/src/main/java/com/ibm/newsapp/CadastroActivity.java
+++ app/src/main/java/com/ibm/newsapp/CadastroActivity.java
@@ -2,6 +2,7 @@ package com.ibm.newsapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -9,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -16,12 +19,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CadastroActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextEmail, editTextPassword;
     private androidx.appcompat.widget.AppCompatButton bt_entrar3;
     String[] mensagens = {"Por favor, preencha todos os campos.", "Cadastro realizado com sucesso!"};
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,7 @@ public class CadastroActivity extends AppCompatActivity {
         bt_entrar3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nome = editTextEmail.getText().toString();
+                String nome = editTextNome.getText().toString();
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
 
@@ -49,7 +58,7 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     private void RegisterUser(View view){
-        String nome = editTextEmail.getText().toString();
+        String nome = editTextNome.getText().toString();
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
 
@@ -58,6 +67,10 @@ public class CadastroActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
+
+                    //SALVANDO DADOS DO USUÁRIO, COMO O NOME, NO NOSSO CASO.
+                    SaveUserData();
+
                     Snackbar snackbar = Snackbar.make(view,mensagens[1],Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.rgb(82, 12, 97));
                     snackbar.setTextColor(Color.WHITE);
@@ -84,6 +97,32 @@ public class CadastroActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //TODO: VER PQ NAO TA SALVANDO A COLECAO NO CONSOLE
+    private void SaveUserData(){
+        String nome = editTextNome.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //GUARDANDO OS DADOS DO USUÁRIO USANDO MAP
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuarios").document(userID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db", "sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db error", "Erro ao salvar os dados" + e.toString());
+            }
+        });
+
+
     }
 
     private void startComponents(){
